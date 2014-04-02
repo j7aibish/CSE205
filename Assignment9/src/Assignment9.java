205082b8b8ce18ff6b134c3d78af80965edb9dda
mkuna <mkuna@asu.edu>
2014-04-01 20:58:15 -0700

    Update 2

--- Assignment9/src/Assignment9.java
+++ Assignment9/src/Assignment9.java
@@ -1,31 +1,51 @@
 //
+// MARTIN A. KUNA
 //
-//
+import java.text.DecimalFormat;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Assignment9 {
 
-	public static void main(String [] args){
+	public static void main(String [] args) throws IOException {
 		
+		double[] array = new double[100];
+		double input;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int i = 0;
+		do{
+			String readLine = reader.readLine();
+			input = Double.parseDouble(readLine);
+			array[i] = input;
+			i++;
+		} while ( input != 0 ); // while input is anything but 0.
+
+		DecimalFormat fmt = new DecimalFormat("0.##");
+		
+		System.out.println("The maximum number is "+fmt.format(findMax(array, 0)));
+		System.out.println("The total number of negative numbers is "+countNegative(array, 0));
+		System.out.println("The sum of positive numbers is "+fmt.format(computeSumPositive(array, 0)));
 	}
-	public static double findMax(double[]a, int b){
-		double max = a[0];
-		for (int i=1; i<a.length; i++){
-			if(a[i]>max){
-				max=a[i];
-				//recursion -> what is my base case
-			}
-			if(i==0){
-				return a[0];
-			}else{
-				max=findMax(a,i-1);
-				if (a[i]>max){
-					return a[i];
-				}
-			}
-		}
+
+	public static double findMax(double[]a, int i){ //not yet working
+		if(i == a.length) return 0;
+		double max=a[i];
+		return (a[i] > max ? 1 : 0) + findMax(a, i+1);
 	}
 	
-	public int linkedTotal(args){
-		return args.length;
+	public static int countNegative(double[]a,int i){
+		if(i == a.length) return 0;
+	    return (a[i] < 0 ? 1 : 0) + countNegative(a, i+1);
+	}
+	
+	public static double computeSumPositive(double[]a, int i) {
+		if(i == a.length) return 0;
+		return (a[i] > 0 ? a[i] : 0) + computeSumPositive(a, i+1);
 	}
 }
+//
+//MARTIN A. KUNA
+//
