54a1709f5ae7c2a4a07f2a83df594ba471c1477a
mkuna <mkuna@asu.edu>
2014-04-01 21:26:24 -0700

    Final Copy

--- Assignment9/src/Assignment9.java
+++ Assignment9/src/Assignment9.java
@@ -15,10 +15,10 @@ import java.io.InputStreamReader;
 public class Assignment9 {
 
 	public static void main(String [] args) throws IOException {
-		
+
 		double[] array = new double[100];
 		double input;
-		
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 		int i = 0;
@@ -30,7 +30,7 @@ public class Assignment9 {
 		} while ( input != 0 );
 
 		DecimalFormat fmt = new DecimalFormat("0.##");
-		
+
 		System.out.println("The maximum number is "+fmt.format(findMax(array, 0)));
 		System.out.println("The total number of negative numbers is "+countNegative(array, 0));
 		System.out.println("The sum of positive numbers is "+fmt.format(computeSumPositive(array, 0)));
@@ -38,14 +38,14 @@ public class Assignment9 {
 
 	public static double findMax(double[]a, int i){
 		if(i == a.length) return 0;
-        return Math.max(a[i], findMax(a, i+1));
+		return Math.max(a[i], findMax(a, i+1));
 	}
-	
+
 	public static int countNegative(double[]a,int i){
 		if(i == a.length) return 0;
-	    return (a[i] < 0 ? 1 : 0) + countNegative(a, i+1);
+		return (a[i] < 0 ? 1 : 0) + countNegative(a, i+1);
 	}
-	
+
 	public static double computeSumPositive(double[]a, int i) {
 		if(i == a.length) return 0;
 		return (a[i] > 0 ? a[i] : 0) + computeSumPositive(a, i+1);
