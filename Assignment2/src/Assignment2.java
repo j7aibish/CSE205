c9c443c77199db059e971acdca923859b826e5ec
Martin Kuna <martinkuna@users.noreply.github.com>
2014-04-01 21:56:40 -0700

    Final Version
    There are A LOT of cluttered comments explaining everything to help YOU learn.

--- Assignment2/src/Assignment2.java
+++ Assignment2/src/Assignment2.java
@@ -11,9 +11,9 @@ public class Assignment2 {
 
 	public static void main(String[] args){
 
-		Scanner in = new Scanner(System.in);  //creates a new scanner object named in
-		int count = 0;	//create new integer named count and initializes it to 0, will be used to keep track of how many numbers the user enters.
-		boolean endLoop = false;	//creates a boolean variable named endLoop and initializes it to default to false
+		Scanner in = new Scanner(System.in);  	//creates a new scanner object named in
+		int count = 0;				//create new integer named count and initializes it to 0, will be used to keep track of how many numbers the user enters.
+		boolean endLoop = false;		//creates a boolean variable named endLoop and initializes it to default to false
 		double [] array = new double[100];	//creates an array with a size of 100, as per the instructions of the assignment saying to be able to handle up to 100 numbers.
 
 		for(int i=0; endLoop!=true; i++){	//iterates so long as endLoop is not true
@@ -22,20 +22,27 @@ public class Assignment2 {
 			if(array[i]!=0){	//every time a value that is not zero is added to the array
 				count++;	//we increase count by 1.
 			} else {
-				endLoop=true; //otherwise, we get out of the loop by setting endLoop to true
+				endLoop=true; 	//otherwise, we get out of the loop by setting endLoop to true
 			}
 		}
 		in.close();  //Closes the scanner object, as it is no longer needed.
 
-		System.out.printf("The minimum number is "+"%.0f", findMin(array,count));  //prints and formats the minimum value of array using findMin
-		System.out.printf("\nThe sum of the positive numbers is $"+"%.2f",computePositiveSum(array,count));  //prints and formats sum of positive numbers using computerPositiveSum
-		System.out.println("\nThe total number of negative numbers is "+countNegative(array,count));  //prints number of negative array values using countNegative
+		//prints and formats the minimum value of array using findMin
+		System.out.printf("The minimum number is "+"%.0f", findMin(array,count));
+		
+		//prints and formats sum of positive numbers using computerPositiveSum
+		System.out.printf("\nThe sum of the positive numbers is $"+"%.2f",computePositiveSum(array,count));
+		
+		//prints number of negative array values using countNegative
+		System.out.println("\nThe total number of negative numbers is "+countNegative(array,count));
 
-	} //end of main method
+	}
 
 	//findMin takes an array, and array length, then finds the lowest/minimum value in array
-	public static double findMin(double[] numbers, int count){ //count is the count of numbers stored in the array
-		double min = numbers[0];  //stores array index 0's value in variable min (as a starting value for our 'for' loop
+	//count is the count of numbers stored in the array
+	public static double findMin(double[] numbers, int count){
+		//stores array index 0's value in variable min (as a starting value for our 'for' loop
+		double min = numbers[0];
 
 		for(int i=0;i<count;i++){		//for loop, iterating until i is less than count(count: number of index's in array, excludes zero)
 			if(numbers[i]<min){		//tests to see if value in the preceding is less than the subsequent number
@@ -47,7 +54,8 @@ public class Assignment2 {
 
 	//computePositiveSum takes an array and array length, then adds up all the positive values
 	public static double computePositiveSum(double[] numbers, int count){
-		double sum=0;	//initializes variable sum to zero
+		//initializes variable sum to zero
+		double sum=0;
 
 		for(int i=0;i<count;i++){		//iterates until i is less than count
 			if(numbers[i]>0){		//if number in the i index of the array is greater than zero
@@ -59,7 +67,8 @@ public class Assignment2 {
 
 	//countNegative takes an array and array length, then tallys up how many negative numbers there are
 	public static int countNegative(double[] numbers, int count){
-		int negCount=0;  //creates an integer negCount, initializing it to zero, because of there are no negative numbers, we must return 0
+		//creates an integer negCount, initializing it to zero, because of there are no negative numbers, we must return 0
+		int negCount=0;
 
 		for(int i=0;i<count;i++){	//iterates until i is less than count
 			if(numbers[i]<0){	//if number at index i is less than 0
