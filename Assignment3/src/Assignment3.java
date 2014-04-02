722609442615037c2eed1f0ca0fc3933d6a364db
Martin Kuna <martinkuna@users.noreply.github.com>
2014-04-01 22:03:58 -0700

    Final Version of Stupidity
    derp

---
+++ Assignment3/src/Assignment3.java
@@ -0,0 +1,40 @@
+/*
+Assignment #: 3
+        Name: Martin Kuna
+   StudentID: <ID REMOVED>
+     Lecture: <CLASS REMOVED>
+ Description: This is dumb and you should feel dumb
+
+1. Thou shalt not use a computer to harm other people.
+
+-- Using a computer to corrupt or compromise someone's data/
+hard drive in order to emotionally hurt them is indecent
+and is unacceptable.
+
+
+3. Thou shalt not snoop around in other people's files.
+
+-- Looking through someone else's PC without permission and
+especially with malicious intent could humiliate the other
+person. PC's are PERSONAL computers for a reason. Stealing
+or viewing another persons data is wrong, and is why we use
+strong passwords on our machines.
+
+4. Thou shalt not use a computer to steal.
+
+-- Using a computer to steal someone's identity can ruin
+someone's life. No one should ever get away with identity
+theft, especially not over computer technology.
+
+5. Thou shalt not use a computer to bear false witness.
+
+-- Using a computer to spread untruthful-rumors about a
+Classmate online in order to humiliate them is awful.
+This type of behavior is unacceptable and is punishable.
+
+6. Thou shalt not use or copy software for which you have not paid.
+
+-- Downloading Adobe Photoshop illegally without paying is unacceptable.
+The company that puts out software should be paid for their work, I
+myself would be upset if someone stole my software without proper license.
+*/
